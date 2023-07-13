5d93ff72dc75e8ce75e5a30d48cef914128bf93f
irines <irina.kiselyowa12@gmail.com>
2023-07-13 15:24:30 +0300

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import ErrorPage from "./ErrorPage";
+import Home from "./pages/Home/home";
+import Contacts from "./pages/Contacts/contacts";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="app">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="" element={<ErrorPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
